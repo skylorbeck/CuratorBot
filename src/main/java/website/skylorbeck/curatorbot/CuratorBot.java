e58ca3f143ca1695b792a6375eb7066cee716ca7
Skylortrexler <42103122+Skylortrexler@users.noreply.github.com>
2022-01-14 08:37:21 -0800

    added random number generator

--- src/main/java/website/skylorbeck/curatorbot/CuratorBot.java
+++ src/main/java/website/skylorbeck/curatorbot/CuratorBot.java
@@ -9,6 +9,7 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.managers.EmoteManager;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import net.dv8tion.jda.api.utils.TimeUtil;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 import net.dv8tion.jda.internal.entities.EmoteImpl;
 import net.dv8tion.jda.internal.managers.EmoteManagerImpl;
@@ -17,12 +18,17 @@ import net.dv8tion.jda.internal.requests.Route;
 import org.jetbrains.annotations.NotNull;
 
 import javax.security.auth.login.LoginException;
+import java.time.Instant;
+import java.time.temporal.TemporalField;
+import java.util.Locale;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import java.util.logging.Logger;
 
 public class CuratorBot extends ListenerAdapter {
     private static final Logger log = Logger.getLogger("CuratorBot");
+    private static final Random ran = new Random(System.currentTimeMillis());
 
     public static void main(String[] args) throws LoginException {
         JDABuilder builder = JDABuilder.createDefault(args[0]);
@@ -36,28 +42,51 @@ public class CuratorBot extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
-        String msg = event.getMessage().getContentDisplay();
-        if (!event.getAuthor().isBot() && msg.startsWith("!")) {
-            event.getMessage().delete().queue();
+        String msg = event.getMessage().getContentDisplay().toLowerCase(Locale.ROOT);
+        if (!event.getAuthor().isBot()) {
+            if (msg.startsWith("!")) {
+                msg = msg.substring(1);
+                event.getMessage().delete().queue();
 
-            if (event.getMember().hasPermission(Permission.ADMINISTRATOR)) {
-                if (msg.contains("shutdown")) {
-                    event.getJDA().shutdown();
-                } else if (msg.contains("poll")) {
-                    String[] choices = msg.substring(5).split(",");
-                    StringBuilder message = new StringBuilder("Vote by choosing a reaction!\n");
-                    for (int i = 0; i < choices.length; i++) {
-                        message.append(i + 1).append("\uFE0F\u20E3").append(" ").append(choices[i]).append("\n");
-                    }
-                    event.getChannel().sendMessage(message).queue(sentMessage -> {
+                if (event.getMember().hasPermission(Permission.ADMINISTRATOR)) {
+
+                    if (msg.startsWith("shutdown")) {
+                        event.getJDA().shutdown();
+                    } else if (msg.startsWith("poll")) {
+                        String[] choices = msg.substring(4).split(",");
+                        StringBuilder message = new StringBuilder("Vote by choosing a reaction!\n");
                         for (int i = 0; i < choices.length; i++) {
-                            sentMessage.addReaction((i + 1) + "\uFE0F\u20E3").queue();
+                            message.append(i + 1).append("\uFE0F\u20E3").append(" ").append(choices[i]).append("\n");
                         }
-                    });
-
+                        event.getChannel().sendMessage(message).queue(sentMessage -> {
+                            for (int i = 0; i < choices.length; i++) {
+                                sentMessage.addReaction((i + 1) + "\uFE0F\u20E3").queue();
+                            }
+                        });
+                    }
+                }
+                StringBuilder message = new StringBuilder();
+                if (msg.startsWith("random")) {
+                    msg = msg.substring(6);
+                        int max = 20;
+                        try {
+                            max = Integer.parseInt(msg);
+                        } catch (Exception ignored) {
+                            try {
+                                max = Integer.parseInt(msg.substring(1));
+                            }catch (Exception ignored2){}
+                        }
+                        message.append("Rolling a d").append(max).append(" for ").append(event.getMember().getEffectiveName()).append("\n");
+                        int result = ran.nextInt(max) + 1;
+                        String resultS = result + "";
+                        String[] results = resultS.split("");
+                        for (String s : results) {
+                            message.append(s).append("\uFE0F\u20E3");
+                        }
+                    event.getChannel().sendMessage(message).queue();
                 }
             }
-            super.onMessageReceived(event);
         }
+        super.onMessageReceived(event);
     }
 }
